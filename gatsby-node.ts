d059ddad179db70dc864f5b0c12a9fddd1ba75a9
romeo <romeomeshack34@gmail.com>
2023-07-08 12:34:15 +0100

    Modified

--- gatsby-node.ts
+++
@@ -1,6 +0,0 @@
-import {optimiseImages} from "./src/utils/optimiseImgs"
-
-// Optimise images after Gatsby bootstrap process is done
-exports.onPostBootstrap = async () => {
-    await optimiseImages();
-}
