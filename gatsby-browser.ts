1e754173cadfb43d79781d213d85568198d031a5
romeo <romeomeshack34@gmail.com>
2023-07-10 00:09:57 +0100

    Gatsby browser

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,9 +1,6 @@
 // Tailwind CSS
 import "./src/styles/global.css";
 
-// Custom stylesheet
-import "./src/styles/sass/main.scss";
-
 // Load prism.js theme for code highlighting
 import "prismjs/themes/prism-solarizedlight.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
