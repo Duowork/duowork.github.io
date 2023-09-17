6957e5f2ccd7416ca991d77ca033135ce22d0ffc
romeo <romeomeshack34@gmail.com>
2023-09-17 03:39:23 +0100

    blog post template

--- src/sharedComponents/blogTemplate/post.tsx
+++ src/sharedComponents/blogTemplate/post.tsx
@@ -3,20 +3,103 @@
 
 import React from "react";
 import { Link, graphql } from "gatsby";
-import { GatsbyImage } from "gatsby-plugin-image";
-import parse from "html-react-parser";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import img7 from "../../assets/images/contact-page-image.jpeg";
 
 import Layout from "../../layouts/layout";
 import Head from "../Head";
 
 export const head = () => <Head />;
 
-export default function Posts() {
+export default function Posts({ data }: any) {
+  const post = data.post;
+
+  const postExcerpt = { __html: post.excerpt };
+  const postContent = { __html: post.content };
+  const bannerImage: any = getImage(post.featuredImage.node.gatsbyImage);
+  // const authorImage =  getImage(post.author.featuredImage.node.gatsbyImage)
+
+  const postTag = post.tags.nodes.length !== 0 ? post.tags.nodes[0].name : "";
+
+  // console.log(post);
+
   return (
     <Layout>
-      <article id="blog-post-container">
-        <header id="post-header"></header>
-        <section id="post-content"></section>
+      <article
+        id="blog-post-container"
+        className="flex flex-col items-center mb-60"
+      >
+        <header id="post-header" className="mb-10 max-w-[700px]">
+          <div className="breadcrumb-nav mb-20 pt-5 text-center text-sm">
+            <Link to="/" className="custom-text-dark">
+              Home
+            </Link>
+            <small className="mx-1 text-gray-300">{">"}</small>{" "}
+            <Link to="/blog">
+              <small>Blog</small>
+            </Link>
+            <small className="mx-1 text-gray-300">{">"}</small>{" "}
+            <small className="text-[#939498] line-clamp-1">{post.title}</small>
+          </div>
+
+          <span
+            id="post-highlight"
+            className={
+              post.tags.nodes.length !== 0
+                ? "custom-bg-green-accent | text-[0.8rem] rounded !px-2 !py-1 capitalize"
+                : ""
+            }
+          >
+            {postTag}
+          </span>
+
+          <h1
+            id="post-title"
+            className="text-2xl sm:text-3xl font-semibold my-5 max-w-[600px]"
+          >
+            {post.title}
+          </h1>
+
+          <div
+            id="post-author-and-date"
+            className="flex flex-col sm:flex-row items-center my-5"
+          >
+            <div id="post-author" className="mr-5 flex items-center">
+              <img
+                src={img7}
+                alt="image"
+                className="w-10 h-10 rounded-full ml-2 object-cover mr-2"
+              />{" "}
+              <small>{post.author.node.name}</small>
+            </div>
+
+            <div id="post-date">
+              {/* Check for post has been update the present update date */}
+              <small className="text-gray-400">
+                {post.date}
+                {/* (updated July 01, 2023) */}
+              </small>
+            </div>
+          </div>
+
+          <GatsbyImage
+            image={bannerImage}
+            className="rounded-md"
+            alt="image"
+          ></GatsbyImage>
+
+          <small
+            id="post-excerpt"
+            className="block text-[0.8rem] my-5 w"
+            dangerouslySetInnerHTML={postExcerpt}
+          />
+        </header>
+
+        <section
+          id="post-content"
+          className="max-w-[700px]"
+          dangerouslySetInnerHTML={postContent}
+        />
       </article>
     </Layout>
   );
@@ -29,10 +112,18 @@ export const postPageQuery = graphql`
     post: wpPost(id: { eq: $id }) {
       id
       title
+      slug
       excerpt
-      status
-      uri
+      content
       date(formatString: "MMMM DD, Y")
+      featuredImage {
+        node {
+          altText
+          caption
+          gatsbyImage(width: 700, height: 400, fit: COVER, placeholder: BLURRED)
+        }
+      }
+      status
       tags {
         nodes {
           name
@@ -43,19 +134,11 @@ export const postPageQuery = graphql`
           name
         }
       }
-      featuredImage {
-        node {
-          altText
-          caption
-          gatsbyImage(width: 100, height: 100, fit: COVER, placeholder: BLURRED)
-        }
-      }
       author {
         node {
           name
         }
       }
-      content
     }
   }
 `;
