f4d4c4eda5fed1c43eec95602cda458120554fe0
romeo <romeomeshack34@gmail.com>
2023-09-22 14:37:26 +0100

    Blog categories images types

--- src/data/blogCategoriesImages.ts
+++ src/data/blogCategoriesImages.ts
@@ -7,7 +7,7 @@ const blogCategoriesImages: BlogCategoriesImagesType = {
     "https://images.pexels.com/photos/1181359/pexels-photo-1181359.jpeg?auto=compress&cs=tinysrgb&w=800",
   startup:
     "https://images.pexels.com/photos/7550298/pexels-photo-7550298.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    "use-case": "https://images.pexels.com/photos/8973487/pexels-photo-8973487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "use-cases": "https://images.pexels.com/photos/8973487/pexels-photo-8973487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   duowork: "/Duowork.webp",
   founder:
     "https://images.pexels.com/photos/8067769/pexels-photo-8067769.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
