60d03fc8f343f3fba90858828d738ad01a00db5a
romeo <romeomeshack34@gmail.com>
2023-09-27 04:58:30 +0100

    Modified

--- src/sharedComponents/SEO.tsx
+++ src/sharedComponents/SEO.tsx
@@ -1,6 +1,6 @@
 /*
   Gatsby uses this component to insert data in  
-  the <head> elmenent of the HMTL document. This helps improve SEO
+  the <head> elmenent of the HTML document. This helps improve SEO
 */
 
 import React from "react";
