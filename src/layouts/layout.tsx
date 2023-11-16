a1604b4fbc9a17c1df750d6fb8b8a1387d053001
romeo <romeomeshack34@gmail.com>
2023-11-16 15:31:37 +0100

    Added custom cursor

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -56,11 +56,11 @@ export default function Layout({ children }: any) {
       />
 
       {/* Custom cursor elements */}
-      {/* <div className="cursor-dot text-white" ref={cursorDotRef}></div>
+      <div className="cursor-dot text-white" ref={cursorDotRef}></div>
       <div
         className="cursor-dot-outline text-white"
         ref={cursorDotOutlineRef}
-      ></div> */}
+      ></div>
     </main>
   );
 }
