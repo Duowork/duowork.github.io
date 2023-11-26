4a206eb19df380ed39401bb80e8ae52bbe0365a3
romeo <romeomeshack34@gmail.com>
2023-11-26 21:33:58 +0100

    Added authors data

--- src/data/blogCategoriesImages.ts
+++ src/data/blogCategoriesImages.ts
@@ -14,7 +14,7 @@ const blogCategoriesImages: BlogCategoriesImagesType = {
     "https://images.pexels.com/photos/8067769/pexels-photo-8067769.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   marketing:
     "https://images.pexels.com/photos/935949/pexels-photo-935949.jpeg?auto=compress&cs=tinysrgb&w=800",
+  seo: "https://images.pexels.com/photos/9822732/pexels-photo-9822732.jpeg?auto=compress&cs=tinysrgb&w=800",
 };
-
 
 export default blogCategoriesImages;
