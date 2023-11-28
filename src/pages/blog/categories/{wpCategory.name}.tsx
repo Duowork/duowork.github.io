e0f5731c9de5b4db801daa8233c37032ffa08861
romeo <romeomeshack34@gmail.com>
2023-11-28 03:43:57 +0100

    Category for SEO and Brand

--- src/pages/blog/categories/{wpCategory.name}.tsx
+++ src/pages/blog/categories/{wpCategory.name}.tsx
@@ -21,6 +21,10 @@ export default function categoryBlogs({ data, params }: any) {
         return categoryLongDescription[categoryName];
       case "software development":
         return categoryLongDescription[categoryName];
+      case "seo":
+        return categoryLongDescription[categoryName];
+      case "brand":
+        return categoryLongDescription[categoryName];
       case "startup":
         return categoryLongDescription[categoryName];
       case "founder":
