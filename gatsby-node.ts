9a7eae4f776d787cd8b94c893a8a661961e60ba8
romeo <romeomeshack34@gmail.com>
2024-01-04 18:00:58 +0100

    Self-signed cert error

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -3,6 +3,9 @@
 const path = require("path");
 const { slash } = require("gatsby-core-utils");
 
+// Self-signed certificate error
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+
 exports.createPages = async ({ graphql, actions }: any) => {
   const { createPage } = actions;
 
