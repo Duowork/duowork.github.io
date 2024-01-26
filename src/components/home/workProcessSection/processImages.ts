250cb138556def2dbef8fac401ca646c19a2a31a
romeo <romeomeshack34@gmail.com>
2024-01-26 13:02:38 +0100

    Process section

---
+++ src/components/home/workProcessSection/processImages.ts
@@ -0,0 +1,10 @@
+import prototypeImage from "./images/design-prototype-icon.png";
+import discoveryImage from "./images/discovery-icon.png";
+import launchImage from "./images/launch-training-maintenance-icon.png";
+import developmentImage from "./images/project-development-icon.png"
+
+export default function () {
+  return {
+   prototypeImage,discoveryImage,launchImage,developmentImage
+  };
+}
