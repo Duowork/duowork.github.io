4f2c39264d0da46de8e298d6ecc0d937ca66ee26
romeo <romeomeshack34@gmail.com>
2024-01-26 13:28:57 +0100

    Portfolio section

---
+++ src/components/portfolioSection/portfolioImages.ts
@@ -0,0 +1,11 @@
+import gobeaconImage from "./images/gobeacon-mockup.jpeg";
+import larksPodcastImage from "./images/larks-mockup.jpeg";
+import aeraveImage from "./images/aerave-mockup.jpg";
+
+export default function () {
+  return {
+    gobeaconImage,
+    larksPodcastImage,
+    aeraveImage,
+  };
+}
