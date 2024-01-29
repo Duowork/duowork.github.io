0da18b58191e90b788d84216a22ed6fe78e5a7b3
romeo <romeomeshack34@gmail.com>
2024-01-29 14:45:05 +0100

    Service section

---
+++ src/components/home/serviceSection/serviceImages.ts
@@ -0,0 +1,11 @@
+import uIUxIcon from "./images/ui-ux.png";
+import mobilDevIcon from "./images/mobile-dev.png";
+import feDevIcon from "./images/fe-dev.png";
+import beDevIcon from "./images/be-dev.png";
+import fsDevIcon from "./images/fs-dev.png"
+
+export default function () {
+  return {
+    uIUxIcon,mobilDevIcon,feDevIcon,beDevIcon,fsDevIcon
+  };
+}
