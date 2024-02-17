47ca93799fd41ce40a3d831b4be276a4bf730957
romeo <romeomeshack34@gmail.com>
2024-02-17 13:37:46 +0100

    Form data collection

---
+++ src/components/contact/formDataType.ts
@@ -0,0 +1,8 @@
+export default interface FormDataType {
+    fullName: string;
+    clientEmail: string;
+    serviceType: string;
+    serviceBudget: string;
+    survey: string;
+    serviceDescription: string;
+}
