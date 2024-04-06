24d2ecda0774562b8975257ac53b0fcef2d8c3bf
romeo <romeomeshack34@gmail.com>
2024-04-06 10:07:42 +0100

    Modified

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,3 +2,12 @@
 /// <reference types="astro/client" />
 
 /// <reference types="astro/astro-jsx" />
+
+// Env variables intelliSense
+interface ImportMetaEnv {
+    readonly ASTRO_HEROTU_API_KEY: string
+}
+
+interface ImportMet {
+    readonly env: ImportMetaEnv
+}
