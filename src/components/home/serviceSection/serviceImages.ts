829085c05e51e8632223451382ba8c13057ad2f7
romeo <romeomeshack34@gmail.com>
2024-08-08 08:12:35 +0100

    Modified

--- src/components/home/serviceSection/serviceImages.ts
+++ src/components/home/serviceSection/serviceImages.ts
@@ -2,10 +2,18 @@ import uIUxIcon from "./images/ui-ux.png";
 import mobilDevIcon from "./images/mobile-dev.png";
 import feDevIcon from "./images/fe-dev.png";
 import beDevIcon from "./images/be-dev.png";
-import fsDevIcon from "./images/fs-dev.png"
+import fsDevIcon from "./images/fs-dev.png";
+import googleAutomation from "./images/google-automation.svg";
+import googleAutomation2 from "./images/google-logo-bold.svg";
 
 export default function () {
   return {
-    uIUxIcon,mobilDevIcon,feDevIcon,beDevIcon,fsDevIcon
+    uIUxIcon,
+    mobilDevIcon,
+    feDevIcon,
+    beDevIcon,
+    fsDevIcon,
+    googleAutomation,
+    googleAutomation2
   };
 }
