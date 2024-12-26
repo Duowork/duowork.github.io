4a944400c509e2d4e8552700730c8ab10b3e2535
romeo <romeomeshack34@gmail.com>
2024-12-26 10:39:20 +0100

    New Images

--- src/components/home/portfolioSection/portfolioImages.ts
+++ src/components/home/portfolioSection/portfolioImages.ts
@@ -1,11 +1,31 @@
 import gobeaconImage from "./images/gobeacon-mockup.jpeg";
 import larksPodcastImage from "./images/larks-mockup.jpeg";
 import aeraveImage from "./images/aerave-mockup.jpg";
+import aeraveLive from "./images/aerave-live.png";
+import avataLive from "./images/avata-live.png";
+import ecomspyLive from "./images/ecomspy-live.png";
+import aiRevolution from "./images/ai-revolution-ui.png";
+import aiRevolution2 from "./images/ai-revolution2-ui.png";
+import allure from "./images/allure-ui.jpeg";
+import allure2 from "./images/allure2-ui.png";
+import NftApp from "./images/nft-app-ui.png";
+import socialApp from "./images/social-app-ui.png";
+import techLaunch from "./images/techlaunch-ui.png";
 
 export default function () {
   return {
     gobeaconImage,
     larksPodcastImage,
     aeraveImage,
+    aeraveLive,
+    avataLive,
+    ecomspyLive,
+    aiRevolution,
+    aiRevolution2,
+    allure,
+    allure2,
+    NftApp,
+    socialApp,
+    techLaunch
   };
 }
